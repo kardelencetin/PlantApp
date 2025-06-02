97266a2d26ba7bdc9f96c77b2d241b5c2f36d515
kardelencetin <krdlnctn98@gmail.com>
2025-06-02 13:52:36 +0300

    paywall screen ui edited. onboarding screen ui edited. all project edited.

--- app/src/main/java/com/kardelencetin/plantapp/feature/paywall/model/PaywallOption.kt
+++ app/src/main/java/com/kardelencetin/plantapp/feature/paywall/model/PaywallOption.kt
@@ -2,5 +2,6 @@ package com.kardelencetin.plantapp.feature.paywall.model
 
 data class PaywallOption(
     val title: String,
-    val description: String
+    val description: String,
+    val hasBadge: Boolean
 )
